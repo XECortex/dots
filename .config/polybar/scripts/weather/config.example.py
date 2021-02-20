8c9958bb88f9a8d2209bfe2930cd1df4461e4be1
XECortex <57626963+XECortex@users.noreply.github.com>
2021-02-20 18:18:37 +0100

    Better weather module configuration file

---
+++ .config/polybar/scripts/weather/config.example.py
@@ -0,0 +1,41 @@
+
+# Polybar python weather module configuration
+
+# OpenWeatherMap
+# 
+# You can get an API key for free by registering at https://openweathermap.org/api/
+# To get the id of your city, search for it on https://openweathermap.org/ and copy the last part of the URL (7 digits long)
+key = "xxxxxxxxxxxxxxxxxxxxxxxx"
+city = "1234567"
+
+# Units and localization
+#
+# Can be "metric" or "imperial"
+# You can freely choose which symbol is displayed after the temperature
+units = "metric"
+symbol = "°C"
+# Set your language as a two-digit code
+lang = "en"
+
+# Icons
+#
+icons = {
+    '01d': '󰖙', # Clear sky
+    '01n': '󰖔', #
+    '02d': '󰖕', # Few clouds (11-25%)
+    '02n': '󰼱', #
+    '03d': '󰖐', # Scattered clouds (25-50%)
+    '03n': '󰖐', #
+    '04d': '󰅟', # Broken / Overcast clouds (51-84% / 85-100%)
+    '04n': '󰅟', #
+    '09d': '󰖖', # Shower rain
+    '09n': '󰖖', #
+    '10d': '', # Moderate / heavy rain
+    '10n': '', #
+    '11d': '󰙾', # Thunderstorm
+    '11n': '󰙾', #
+    '13d': '󰼶', # Snow
+    '13n': '󰼶', #
+    '50d': '󰖑', # Fog
+    '50n': '󰖑'  #
+}
---
+++ .config/polybar/scripts/weather/weather.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+if not os.path.isfile(f"{path}/config.py"):
+    print(f"⚠ No weather config found. Check out \"{path}/config.py\"")
+    exit()
+else:
+    from config import *
+
+url = f"https://api.openweathermap.org/data/2.5/weather?id={city}&appid={key}&units={units}&lang={lang}"
+res = requests.get(url)
+icon = icons.get(res.json().get('weather')[0]['icon'], "󰼯")
+temp = round(res.json().get('main')['temp'], 1)
+
+print(icon, temp, symbol)
